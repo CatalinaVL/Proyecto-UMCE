85f5c3147794e23fe200d4696db506ebb6ba9a5c
CatalinaVL <catalina.vidal@ug.uchile.cl>
2024-01-03 00:36:54 -0300

    C

--- ANALISIS.R
+++ ANALISIS.R
@@ -10,8 +10,8 @@ RENISE20 <- fread("C:/Users/cata_/Documents/UMCE2/RENISE/RENISE 2020.csv")
 RENISE21 <- fread("C:/Users/cata_/Documents/UMCE2/RENISE/RENISE 2021.csv")
 RENISE22 <- fread("C:/Users/cata_/Documents/UMCE2/RENISE/RENISE 2022.csv")
 RENISE23 <- fread("C:/Users/cata_/Documents/UMCE2/RENISE/RENISE 2023.csv")
-RENISE <- fread("C:/Users/cata_/Documents/UMCE2/RENISE.csv")
-rm(RENISE2)
+RENISE <- fread("C:/Users/cata_/Documents/UMCE2/RENISE2.csv")
+
 #----- Obtener las categorías únicas de la variable antecedente_1 -----
 names(RENISE17)
 resp <- unique(RENISE15$HIJOS)
@@ -21,12 +21,14 @@ resp <- unique(RENISE15$GRUPO_FONASA)
 resp <- unique(RENISE15$ID_CHS_MDS)
 resp <- unique(RENISE15$FONASAPOBRE12)
 resp <- unique(RENISE15$SOBREDAD12)
-resp <- unique(RENISE16$SENAME)
+resp <- unique(RENISE15$SENAME)
+resp2 <- unique(RENISE15$HIJOS)
 
 # Mostrar las categorías únicas
 print(resp)
+print(resp2)
 
-#Seleccionar variables 
+#Seleccionar variables -----
 names(RENISE15)
 RENISE15 <- select(RENISE15,
                    AÑO = ID_ANO_ESCOLAR,
@@ -90,8 +92,9 @@ RENISE23 <- select(RENISE23,
                    HIJOS = CONHIJOS,
                    ESC_MADRE = ESC_MADRE,
                    SENAME = SENAME_2013_2022)
+
 #Escolaridad madre----
-RENISE15 <- RENISE15 %>%
+RENISE17 <- RENISE17 %>%
   mutate(ESC_MADRE2 = case_when(
     ESC_MADRE %in% 1:8 ~ "BASICA",
     ESC_MADRE %in% 9:12 ~ "MEDIA",
@@ -99,42 +102,37 @@ RENISE15 <- RENISE15 %>%
     TRUE ~ as.character(ESC_MADRE)  # Si no cumple ninguna condición, conserva el valor original como texto
   ))
 
-ESCOLARIDADMADRE <- RENISE15 %>%
+ESCOLARIDADMADRE <- RENISE17 %>%
   group_by(ID_RBD) %>%
-  summarise(ESC_BASICA = mean(ESC_MADRE2 == "BASICA", na.rm = TRUE) * 100,
-            ESC_MEDIA = mean(ESC_MADRE2 == "MEDIA", na.rm = TRUE) * 100,
-            ESC_SUPERIOR = mean(ESC_MADRE2 == "SUPERIOR", na.rm = TRUE) * 100)
+  summarise(ESC_BASICA = mean(ESC_MADRE2 == "BASICA", na.rm = TRUE),
+            ESC_MEDIA = mean(ESC_MADRE2 == "MEDIA", na.rm = TRUE),
+            ESC_SUPERIOR = mean(ESC_MADRE2 == "SUPERIOR", na.rm = TRUE))
 
-ESCOLARIDADMADRE <- ESCOLARIDADMADRE %>%
-  mutate(AÑO = 2014)
+ESCOLARIDADMADRE[is.na(ESCOLARIDADMADRE)] <- NA
 
 #HIJOS
-RENISE15$HIJOS <- ifelse(is.na(RENISE15$HIJOS), 0, 1)
+RENISE17$HIJOS <- ifelse(is.na(RENISE17$HIJOS), 0, 1)
 
-HIJOS <- RENISE15 %>%
+HIJOS <- RENISE17 %>%
   group_by(ID_RBD) %>%
-  summarise(NOHIJOS = mean(HIJOS == 0, na.rm = TRUE) * 100,
-            HIJOS = mean(HIJOS == 1, na.rm = TRUE) * 100)
+  summarise(NOHIJOS = mean(HIJOS, na.rm = TRUE))
 
-HIJOS <- HIJOS %>%
-  mutate(AÑO = 2014)
+
 #SENAME 
-RENISE15$SENAME <- ifelse(is.na(RENISE15$SENAME), 0, 1)
+RENISE17$SENAME <- ifelse(is.na(RENISE17$SENAME), 0, 1)
 
-SENAME <- RENISE15 %>%
+SENAME <- RENISE17 %>%
   group_by(ID_RBD) %>%
-  summarise(NOSENAME = mean(SENAME == 0, na.rm = TRUE) * 100,
-            SENAME = mean(SENAME == 1, na.rm = TRUE) * 100)
+  summarise(SENAME = mean(RENISE17$SENAME, na.rm = TRUE))
 
-SENAME <- SENAME %>%
-  mutate(AÑO = 2014)
+#UNIR
+RENISE17 <- left_join(ESCOLARIDADMADRE, HIJOS)
+RENISE17 <- left_join(RENISE17, SENAME)
 
-
-
-
-RENISE15 <- left_join(ESCOLARIDADMADRE, HIJOS)
-RENISE15 <- left_join(RENISE15, SENAME)
-
+RENISE17 <- RENISE17 %>%
+  mutate(AÑO= 2022)
+RENISE17 <- RENISE17 %>%
+  mutate(AÑOLOGICO = 2023)
 
 RENISE <- rbindlist(list(RENISE15, RENISE16), fill = TRUE)
 RENISE <- rbindlist(list(RENISE, RENISE17), fill = TRUE)
@@ -145,14 +143,73 @@ RENISE <- rbindlist(list(RENISE, RENISE21), fill = TRUE)
 RENISE <- rbindlist(list(RENISE, RENISE22), fill = TRUE)
 RENISE <- rbindlist(list(RENISE, RENISE23), fill = TRUE)
 
+names(RENISE)
+#ORDENAR NOMBRES
+RENISE <- RENISE %>%
+  select(ID_RBD, AÑO, AÑOLOGICO, ESC_BASICA, ESC_MEDIA, ESC_SUPERIOR, HIJOS, SENAME)
+# Renombrar la variable "NOHIJOS" a "HIJOS" en el DataFrame RENISE23
+RENISE <- RENISE %>%
+  rename(HIJOS = NOHIJOS)
+
+
+
 #GUARDAR
 fwrite(RENISE, "C:/Users/cata_/Documents/UMCE2/RENISE2.csv")
+
+
+#-----
+
+# Escolaridad madre y características HIJOS y SENAME para RENISE21
+# Escolaridad madre y características HIJOS y SENAME para RENISE22
+# Escolaridad madre y características HIJOS y SENAME para RENISE23
+RENISE23 <- RENISE23 %>%
+  mutate(ESC_MADRE2 = case_when(
+    ESC_MADRE %in% 1:8 ~ "BASICA",
+    ESC_MADRE %in% 9:12 ~ "MEDIA",
+    ESC_MADRE > 12 ~ "SUPERIOR",
+    TRUE ~ as.character(ESC_MADRE)
+  ))
+
+ESCOLARIDADMADRE <- RENISE23 %>%
+  group_by(ID_RBD) %>%
+  summarise(ESC_BASICA = mean(ESC_MADRE2 == "BASICA", na.rm = TRUE),
+            ESC_MEDIA = mean(ESC_MADRE2 == "MEDIA", na.rm = TRUE),
+            ESC_SUPERIOR = mean(ESC_MADRE2 == "SUPERIOR", na.rm = TRUE))
+
+ESCOLARIDADMADRE[is.na(ESCOLARIDADMADRE)] <- NA
+
+RENISE23$HIJOS <- ifelse(is.na(RENISE23$HIJOS), 0, 1)
+
+HIJOS <- RENISE23 %>%
+  group_by(ID_RBD) %>%
+  summarise(NOHIJOS = mean(HIJOS, na.rm = TRUE))
+
+RENISE23$SENAME <- ifelse(is.na(RENISE23$SENAME), 0, 1)
+
+SENAME <- RENISE23 %>%
+  group_by(ID_RBD) %>%
+  summarise(SENAME = mean(SENAME, na.rm = TRUE))
+
+RENISE23 <- left_join(ESCOLARIDADMADRE, HIJOS)
+RENISE23 <- left_join(RENISE23, SENAME)
+
+RENISE23 <- RENISE23 %>%
+  mutate(AÑO = 2022) %>%
+  mutate(AÑOLOGICO = 2023)
 
 
 
 
 #----
   
+lista_bases <- split(RENISE, RENISE$AÑOLOGICO)
+RENISE_2023 <- lista_bases$"2023"  # Cambia "2015" por el año que necesites
+duplicados_2015 <- sum(duplicated(RENISE_2023$ID_RBD))
+duplicados_2015 <- subset(RENISE_2015, duplicated(RENISE_2015$ID_RBD) | duplicated(RENISE_2015$ID_RBD, fromLast = TRUE))
+RENISE_2022[duplicated(RENISE_2022[, c("ID_RBD", "AÑO")]), ]
+RENISE23[duplicated(RENISE23),]
+
+
 
 # Calcular el promedio por grupo fonsa para cada colegio----
 
